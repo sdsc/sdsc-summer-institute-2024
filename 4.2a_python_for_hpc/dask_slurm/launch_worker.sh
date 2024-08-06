c6440c9c7c888116f81127d0cbe00fdc85dc932a
Andrea Zonca <code@andreazonca.com>
2024-08-06 14:17:01 -0700

    singularity moved from script to slurm

--- 4.2a_python_for_hpc/dask_slurm/launch_worker.sh
+++ 4.2a_python_for_hpc/dask_slurm/launch_worker.sh
@@ -1,7 +1,5 @@
 echo "Launching dask worker"
 MEM_GB=240
-SIMG='/expanse/lustre/projects/sds166/zonca/dask-numba-si23.sif'
 # memory limit is in bytes
 MEM=$(( $MEM_GB*1024**3 ))
-module load singularitypro
-singularity exec $SIMG dask worker --scheduler-file ~/.dask_scheduler.json --memory-limit $MEM --nworkers 1
+dask worker --scheduler-file ~/.dask_scheduler.json --memory-limit $MEM --nworkers 1
