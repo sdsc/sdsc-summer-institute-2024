252f37e416824094814b1c4ba620a5664b3744ef
Marty Kandes <mkandes@sdsc.edu>
2024-08-06 11:45:02 -0700

    Update compute-pi-stats.sh

--- 3.5_high_throughput_computing/compute-pi-stats.sh
+++ 3.5_high_throughput_computing/compute-pi-stats.sh
@@ -11,7 +11,7 @@
 #SBATCH --time=00:30:00
 #SBATCH --output=%x.o%j.%N
 
-declare -xir DEPENDENT_SLURM_ARRAY_JOB_ID="$(echo ${SLURM_JOB_DEPENDENCY} | grep -o '[[:digit:]]*')"
+declare -xir ARRAY_JOB_ID="${1}"
 
 module reset
 module load gcc/10.2.0
